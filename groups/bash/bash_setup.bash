bcad58f0754faaf8efa53fd6f03605f0b53954e2
ethanrutt <ethan.rutt2032@gmail.com>
2025-03-26 00:18:57 -0500

    minor fixes

--- groups/bash/bash_setup.bash
+++ groups/bash/bash_setup.bash
@@ -1,28 +1,39 @@
 if [[ -z "$DOTS_DIR" ]] || [[ -z "$ID" ]]; then
-    echo -e "\e[31mDOTS_DIR is an empty string. This file should only be called from dots/main.bash. If not, then there will be undefined variables that are needed to execute properly\e[0m"
+    echo -e "\e[31m\$DOTS_DIR or \$ID is an empty string. This file should only be called from dots/main.bash. If not, then there will be undefined variables that are needed to execute properly.\e[0m"
     exit 1
 fi
 
+echo -e "\n\n${MAGENTA}bash setup${ENDCOLOR}"
+
 if [[ -f "$HOME/.bashrc" ]]; then
+    echo -e "${YELLOW}removing existing .bashrc${ENDCOLOR}"
     rm "$HOME/.bashrc"
 fi
 
 if [[ -f "$HOME/.bash_profile" ]]; then
+    echo -e "${YELLOW}removing existing .bash_profile${ENDCOLOR}"
     rm "$HOME/.bash_profile"
 fi
 
-if [[ -d "$HOME/.config/bash " ]] || [[ -L "$HOME/.config/bash" ]]; then
+if [[ -d "$HOME/.config/bash" ]] || [[ -L "$HOME/.config/bash" ]]; then
+    echo -e "${YELLOW}removing existing .config/bash${ENDCOLOR}"
     rm -r "$HOME/.config/bash"
 fi
 
+echo -e "${YELLOW}linking new .bashrc${ENDCOLOR}"
 ln -s "$DOTS_DIR/groups/bash/files/.bashrc" "$HOME/.bashrc"
+
+echo -e "${YELLOW}linking new .config/bash${ENDCOLOR}"
 ln -s "$DOTS_DIR/groups/bash/files/bash" "$HOME/.config/bash"
 
+echo -e "${YELLOW}linking new .bash_profile${ENDCOLOR}"
 case $ID in
-  ubuntu|debian)
-      ln -s "$DOTS_DIR/groups/bash/files/debian/.bash_profile" "$HOME/.bash_profile"
+    ubuntu|debian)
+        ln -s "$DOTS_DIR/groups/bash/files/debian/.bash_profile" "$HOME/.bash_profile"
     ;;
-  arch)
-      ln -s "$DOTS_DIR/groups/bash/files/arch/.bash_profile" "$HOME/.bash_profile"
+    arch)
+        ln -s "$DOTS_DIR/groups/bash/files/arch/.bash_profile" "$HOME/.bash_profile"
     ;;
 esac
+
+echo -e "${GREEN}bash setup complete${ENDCOLOR}"
--- main.bash
+++ main.bash
@@ -8,46 +8,47 @@ RED="\e[31m" # errors
 GREEN="\e[32m" # successful roles
 YELLOW="\e[33m" # warnings / skips
 BLUE="\e[34m" # pre-setup messages
-MAGENTA="\e[35m"
+MAGENTA="\e[35m" # starting new role
 CYAN="\e[36m" # variables
 ENDCOLOR="\e[0m"
 
 source /etc/os-release
 echo -e "${BLUE}Loading Setup for detected OS:${ENDCOLOR} ${CYAN}${ID}${ENDCOLOR}"
 case $ID in
-  ubuntu|debian)
-    if ! dpkg -s git >/dev/null 2>&1; then
-      echo "${BLUE}Installing git${ENDCOLOR}"
-      sudo apt-get install -y git
-    fi
+    ubuntu|debian)
+        if ! dpkg -s git >/dev/null 2>&1; then
+            echo "${BLUE}Installing git${ENDCOLOR}"
+            sudo apt-get install -y git
+        fi
     ;;
-  arch)
-    echo -e "${BLUE}Setting Locale${ENDCOLOR}"
-    sudo localectl set-locale LANG=en_US.UTF-8
+    arch)
+        echo -e "${BLUE}Setting Locale${ENDCOLOR}"
+        sudo localectl set-locale LANG=en_US.UTF-8
 
-    if ! pacman -Q git >/dev/null 2>&1; then
-      echo "${BLUE}Installing git${ENDCOLOR}"
-      sudo pacman -S --noconfirm git
-    fi
+        if ! pacman -Q git >/dev/null 2>&1; then
+            echo "${BLUE}Installing git${ENDCOLOR}"
+            sudo pacman -S --noconfirm git
+        fi
     ;;
   *)
-    echo -e "${RED}Unsupported OS${ENDCOLOR}"
-    exit 1
-    ;;
+      echo -e "${RED}Unsupported OS${ENDCOLOR}"
+      exit 1
+  ;;
 esac
 
 # FIXME: uncomment
 # if ! [[ -d "$DOTS_DIR" ]]; then
-#   echo -e "${BLUE}Cloning repository${ENDCOLOR}"
-#   git clone --recurse-submodules git@github.com:ethanrutt/dots.git "$DOTS_DIR"
+#     echo -e "${BLUE}Cloning repository${ENDCOLOR}"
+#     git clone --recurse-submodules git@github.com:ethanrutt/dots.git "$DOTS_DIR"
 # else
-#   echo -e "${BLUE}Updating repository${ENDCOLOR}"
-#   git -C "$DOTS_DIR" pull --recurse-submodules --quiet
+#     echo -e "${BLUE}Updating repository${ENDCOLOR}"
+#     git -C "$DOTS_DIR" pull --recurse-submodules --quiet
 # fi
 
 if ! [[ -d "$HOME/.config" ]]; then
-  mkdir "$HOME/.config"
+    mkdir "$HOME/.config"
 fi
 
 # FIXME: need bash dependencies before
+source "$DOTS_DIR/groups/bat/bat_setup.bash"
 source "$DOTS_DIR/groups/bash/bash_setup.bash"
