aa9ec761da19bdac7f5948af341f794edd1ba81b
ethanrutt <ethan.rutt2032@gmail.com>
2025-03-26 12:28:29 -0500

    add wallpaper group

---
+++ groups/wallpapers/wallpapers_setup.bash
@@ -0,0 +1,11 @@
+if [[ -z "$DOTS_DIR" ]] || [[ -z "$ID" ]]; then
+    echo -e "\e[31m\$DOTS_DIR or \$ID is an empty string. This file should only be called from dots/main.bash. If not, then there will be undefined variables that are needed to execute properly.\e[0m"
+    exit 1
+fi
+
+echo -e "\n\n${MAGENTA}wallpapers setup${ENDCOLOR}"
+
+mkdir -p "$HOME/Pictures"
+link_file "$DOTS_DIR/groups/wallpapers/files/wallpapers" "$HOME/Pictures/wallpapers"
+
+echo -e "${GREEN}wallpapers setup complete${ENDCOLOR}"
