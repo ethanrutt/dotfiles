a0e401f7d7af765e5804b9e518f424e856731b83
ethanrutt <ethan.rutt2032@gmail.com>
2025-05-12 13:11:35 -0700

    add browser open capabilities for wsl

---
+++ groups/wsl/wsl_setup.bash
@@ -0,0 +1,12 @@
+if [[ -z "$DOTS_DIR" ]] || [[ -z "$ID" ]]; then
+    echo -e "\e[31m\$DOTS_DIR or \$ID is an empty string. This file should only be called from dots/main.bash. If not, then there will be undefined variables that are needed to execute properly.\e[0m"
+    exit 1
+fi
+
+echo -e "\n\n${MAGENTA}wsl setup${ENDCOLOR}"
+
+sudo add-apt-repository ppa:wslutilities/wslu
+sudo apt update
+install_with_package_manager apt wslu
+
+echo -e "${GREEN}wsl setup complete${ENDCOLOR}"
